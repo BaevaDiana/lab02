f741b3227f6ec63fcd4c2cdee995964e6ba6419c
BaevaDiana <thedianab15@gmail.com>
2023-04-10 23:52:04 +0300

    Try upgrate commemtaries

--- example/hello_world.cpp
+++ example/hello_world.cpp
@@ -1,11 +1,17 @@
-#include <iostream>
-#include <string>
+
+#include <iostream> // подключение библиотеки
+#include <string> // подключение библиотеки 
+
 int
+
 main()
 {
-  std::string name; // объявляем переменную для имени пользователя
-  std::cout << "Enter your name:";
-  std::cin >> name; //считываем имя из стандартного ввода
-  std::cout << "Hello World from @" << name
-            << "\n"; //выводим сообщение с именем пользователя
+std::string name; // объявляем переменную для имени пользователя типа String
+std::cout << "Enter your name:"; //вывод в консоли
+std::cin >> name; //считываем имя из стандартного ввода
+
+std::cout << "Hello World from @" << name
+
+<< "\n"; //выводим сообщение с именем пользователя с правильными проблеми
+
 }
