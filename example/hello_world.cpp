8c03ac51ef6966512d84df21fe4efc48fd2fdd41
BaevaDiana <thedianab15@gmail.com>
2023-04-10 22:12:01 +0300

    updated hello_world.cpp program using patch1

--- example/hello_world.cpp
+++ example/hello_world.cpp
@@ -1,14 +1,9 @@
 #include <iostream>
-
-#include <string>  
-int main() {
-
-std::string name;  // объявляем переменную для имени пользователя
-
-std::cout << "Enter your name: ";
-std::cin >> name;  // считываем имя пользователя из стандартного ввода
-
-std::cout << "Hello world from @" << name << "\n";  // выводим сообщение с именем пользователя
-return 0:
-
+#include <string>
+int main()
+{
+std::string name; // объявляем переменную для имени пользователя
+std:: cout <<"Enter your name:";
+std:: cin >> name; //считываем имя из стандартного ввода
+std:: cout << "Hello World from @" << name << "\n"; //выводим сообщение с именем пользователя
 }
