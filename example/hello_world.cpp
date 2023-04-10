6f154ccdb035c6dd7de62e8586da46e252969543
BaevaDiana <thedianab15@gmail.com>
2023-04-10 23:01:10 +0300

    changed code style in hello_world.cpp

--- example/hello_world.cpp
+++ example/hello_world.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <string>
-int main()
+int
+main()
 {
-std::string name; // объявляем переменную для имени пользователя
-std:: cout <<"Enter your name:";
-std:: cin >> name; //считываем имя из стандартного ввода
-std:: cout << "Hello World from @" << name << "\n"; //выводим сообщение с именем пользователя
+  std::string name; // объявляем переменную для имени пользователя
+  std::cout << "Enter your name:";
+  std::cin >> name; //считываем имя из стандартного ввода
+  std::cout << "Hello World from @" << name
+            << "\n"; //выводим сообщение с именем пользователя
 }
